718e0ee094d0514e8403e938c2d16cace81c1c70
Yukki <frontend.yukki+github@gmail.com>
2021-08-09 16:32:09 +0900

    移動

--- src/index.js
+++ src/index.js
@@ -11,18 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i){
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-  
   renderSquare(i) {
     return (
       <Square
@@ -65,7 +53,19 @@ class Game extends React.Component {
       xIsNext: true,
     };
   }
-  
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
